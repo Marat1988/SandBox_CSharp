e0b74dc101a19af4e324141694583c79b82118a3
Marat <pirat03071988@mail.ru>
2025-03-29 14:11:12 +0200

    FinanceProject

---
+++ FinanceApp/Data/Service/IExtensesService.cs
@@ -0,0 +1,11 @@
+﻿using FinanceApp.Models;
+
+namespace FinanceApp.Data.Service
+{
+    public interface IExtensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+        Task Add(Expense expense);
+        IQueryable GetChartData();
+    }
+}
